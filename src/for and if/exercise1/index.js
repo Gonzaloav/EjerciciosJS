feece44e8f593aadabfa4770cc84188b59abe242
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-21 13:04:25 +0100

    ejercicio1

--- src/for and if/exercise1/index.js
+++ src/for and if/exercise1/index.js
@@ -5,7 +5,12 @@ const even = []
 for (let item of numbers) {
     console.log("item:", item);
 
-    // Put your code here
+    if (item%2===0) {
+        even.push(item);
+    }
+    else {
+        odd.push(item);
+    }
 
 }
 
