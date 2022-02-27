1dc3391d0dbd5b74f52233289b70f3f3828b9f5a
Gonzaloav <Gonzaloav@protonmail.com>
2022-02-27 16:57:44 +0100

    Funcion 1

--- src/functions/exercise2/index.js
+++ src/functions/exercise2/index.js
@@ -1,10 +1,14 @@
-number = 0;
+/**Hay varios problemas en el. Uno tiene que ver con el concepto de ámbito de las variables y 
+ * otro con la necesidad de usar nombres claros y significativos en nuestro código.
+ */
+
+let number = 0;
 
 function plusone(number) {
     number = number + 1;
     return number;
 }
 
-plusone(number);
+plusone(1);
 
 console.log(number)
