165abe884955603c9cd37f68a047cb07ca56d1c4
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-02 10:46:55 +0100

    corregir errores varios

--- src/for and if/exercise10/index.js
+++ src/for and if/exercise10/index.js
@@ -1,3 +1,32 @@
+/**  ordenar los números en un array.  
+ * Compara el primer elemento del array con cada uno de los siguientes. Si alguno de los elementos 
+ * siguientes es más pequeño que el primero, intercambia la posición de ese elemento y el primero. 
+ * Ya tenemos el elemento más pequeño en la primera posición.
+  */ 
+
+
 let numbers = [2,1,4,5,6,3,7,8,1,4,6,8,1,5,7,8,3]
 
-// Put your code here
+const and = 1;
+
+console.log ("Desordenados", numbers)
+
+const left = [numbers]
+const right = [numbers]
+
+    for (let left = 0; left < 9 ; left++) {
+        for (let right = 0; right < 9; right++ ){
+             console.log(`Left: ${left}, Right: ${right}, Resultado: ${left * right}`);
+    }
+}  
+
+            if (numbers [outer] > numbers[left]){
+                let temp = null;
+                temp = numbers[outer];
+                numbers[outer] = numbers [left]
+                numbers[left] = temp;
+                console.log ("proceso", numbers)
+        }
+    }
+}
+console.log ("Ordenados", numbers)
--- src/functions/exercise1/index.js
+++ src/functions/exercise1/index.js
@@ -1,3 +1,8 @@
+/**una función que busque y entregue (return) el menor valor de un array de números, 
+ * insertando el código necesario en el lugar indicado por el comentario.
+ */
+
+
 const numbers1 = [22,1,4,3,5,7,8,9,12,11];
 const numbers2 = [22,7,4,3,5,7,8,9,12,11];
 const numbers3 = [22,6,4,4,5,7,8,9,12,11];
@@ -6,7 +11,14 @@ const numbers4 = [22,9,6,8,5,7,8,9,12,11];
 function minorValue(array) {
 
     // Put your code here
-
+    
+        let temp = array[0];
+        for(let idx = 1; idx < array.length; idx++){
+            if(array[idx] < temp){
+                temp = array[idx]; 
+            }
+        }
+        return temp   
 }
 
 const minor1 = minorValue(numbers1);
