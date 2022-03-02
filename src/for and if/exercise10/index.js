13843a3687865540eebe84a8bef1b8a82b501b11
Gonzaloav <Gonzaloav@protonmail.com>
2022-03-02 15:24:57 +0100

    corregir ejercicio 10 desde casa

--- src/for and if/exercise10/index.js
+++ src/for and if/exercise10/index.js
@@ -1,19 +1,26 @@
+/**Compara el primer elemento del array con cada uno de los siguientes. 
+ Si alguno de los elementos siguientes es más pequeño que el primero, 
+ intercambia la posición de ese elemento y el primero, y asi con todo el Array 
+ */ 
+ 
 let numbers = [2,1,4,5,6,3,7,8,1,4,6,8,1,5,7,8,3]
 
-console.log("Desordenados", numbers)
+console.log( numbers)
 
-for(let outer = 0; outer < numbers.length; outer++){
-    
-    for(let inner = outer + 1; inner < numbers.length; inner++ ){
-        
-        if(numbers[outer] > numbers[inner]){
+for (let i = 0; i < numbers.length; i++) {
+
+    for (let j = i + 1; j < numbers.length; j++) {
+        if (numbers[i] > numbers[j]) { 
             
-            let temp = null;
-            temp = numbers[outer];
-            numbers[outer] = numbers[inner];
-            numbers[inner] = temp;
-            console.log("proceso", numbers)            
+            let casilla = numbers[i];
+           
+            numbers[i] = numbers[j];
+            
+            numbers[j] = casilla;
+            console.log(numbers)
         }
     }
 }
-console.log("Ordenados", numbers)
+console.log(numbers)
+
+
--- src/functions/exercise1/index.js
+++ src/functions/exercise1/index.js
@@ -1,27 +1,28 @@
-/**una función que busque y entregue (return) el menor valor de un array de números, 
+/** Una función que busque y entregue (return) 
+ * el menor valor de un array de números, 
  * insertando el código necesario en el lugar indicado por el comentario.
- * 
- */
-
+ * Primero resuelvo como hayar el número menor, al recorrer un Array
+*/
 
 const numbers1 = [22,1,4,3,5,7,8,9,12,11];
 const numbers2 = [22,7,4,3,5,7,8,9,12,11];
 const numbers3 = [22,6,4,4,5,7,8,9,12,11];
 const numbers4 = [22,9,6,8,5,7,8,9,12,11];
 
-function minorValue(array) {
+function minorValue(numbers1) { 
+                               
+    let temp = numbers1[0];
 
-    let temp = array[0];
-    for(let idx = 1; idx < array.length; idx++){
-        if(array[idx] < temp){
-            temp = array[idx]; 
+    for (let i = 0; i < numbers1.length; i++) {
+        if (numbers1[i] < temp) {
+           temp = numbers1[i];
         }
     }
-    return temp
+    console.log(temp);
+    return temp; 
 }
 
-
-const minor1 = minorValue(numbers1);
+const minor1 = minorValue(numbers1); 
 console.log(minor1);
 console.log(minorValue(numbers2));
 console.log(minorValue(numbers3));
