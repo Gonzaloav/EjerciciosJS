46166c70fb8df70f1081ee33783c442ca0353798
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-04-01 12:41:38 +0200

    index3

--- src/objects as maps/exercise3/index.js
+++ src/objects as maps/exercise3/index.js
@@ -1,3 +1,12 @@
+/** ¿Has completado el ejercicio 8 de funciones? 
+ * Adapta el código para poder ordenar los corredores del array chrono del ejercicio anterior. 
+ * Pega en index.js tus soluciones del ejercicio 8 de funciones. Sólo deberías de necesitar 
+ * añadir dos pequeñas funciones:	
+ * aFasterThanB(runnerA, runnnerB)	Entrega true si runnerB.
+ * time es mayor que runnerA.time. En otro caso entrega false.		
+ * bFasterThanA(runnerA, runnnerB)
+Entrega true si runnerA.time es mayor que runnerB.time. En otro caso entrega false. */
+
 const people = [
     {
         name: "Ana",
@@ -21,7 +30,34 @@ const people = [
     },
 ]
 
-// Your code here
+function aFasterThanB(a,b) {	  
+         return a > b;	
+}
+
+function bFasterThanA(a, b) {
+    	 return b > a;	
+}
+function swap(runnerA, runnnerB, people) {
+    const temp = people[runnerA].time;
+    people[runnerA].time = people[runnnerB].time;
+    people[runnnerB].time = temp;	
+}
+
+function sort(comparator, people) {
+
+    for (let currentIdx = 0; currentIdx < people.length - 1; currentIdx++) {
+
+        for (let nextIdx = currentIdx + 1; nextIdx < people.length; nextIdx++) {
+
+if ( comparator(people[currentIdx].time, people[nextIdx].time)
+) swap(currentIdx, nextIdx, people)
+	     }	
+    }	
+}
+sort(aFasterThanB, people);	
+console.log(people);
+sort(bFasterThanA, people);	
+console.log(people);
 
 sort(aFasterThanB, numbers)
 sort(bFasterThanA, numbers)
